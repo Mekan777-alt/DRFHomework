a6ae7d65231571112687c0641c7a3cf61e4aa7c8
Mekan777-alt <mekan2678@gmail.com>
2023-12-26 18:30:20 +0300

    change_password

--- backend/users/api/views/change_password.py
+++ backend/users/api/views/change_password.py
@@ -1,11 +1,10 @@
+from django.shortcuts import get_object_or_404
+from rest_framework_simplejwt.tokens import AccessToken
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.decorators import api_view, permission_classes, authentication_classes
 from rest_framework.permissions import IsAuthenticated
 from django.contrib.auth import get_user_model
-from django.contrib.auth.tokens import PasswordResetTokenGenerator
-from django.utils.encoding import force_str
-from django.utils.http import urlsafe_base64_decode
 from django.contrib.auth.hashers import make_password
 from rest_framework_simplejwt.authentication import JWTAuthentication
 from rest_framework_simplejwt.tokens import RefreshToken
@@ -22,9 +21,9 @@ def reset_password(request):
         user = get_user_model().objects.filter(email=email).first()
 
         if user:
-            token = PasswordResetTokenGenerator().make_token(user)
+            token = AccessToken.for_user(user)
 
-            reset_link = f"api/auth/change-password?secret={token}"
+            reset_link = f"api/auth/change-password?secret={str(token)}"
 
             send_password_reset_email.delay(email, reset_link)
 
@@ -34,8 +33,6 @@ def reset_password(request):
 
 
 @api_view(['POST'])
-# @authentication_classes([JWTAuthentication])
-# @permission_classes([IsAuthenticated])
 def change_password(request):
     token = request.GET.get("secret", None)
     password = request.data.get('password')
@@ -43,9 +40,10 @@ def change_password(request):
 
     if token and password == confirm_password:
         try:
-            # Decode the token and get the user ID
-            user_id = force_str(urlsafe_base64_decode(token))
-            user = get_user_model().objects.get(pk=user_id)
+            decoded_token = AccessToken(token)
+            user_id = decoded_token['user_id']
+
+            user = get_object_or_404(get_user_model(), pk=user_id)
 
             user.password = make_password(password)
             user.save()
@@ -55,7 +53,7 @@ def change_password(request):
 
             return Response({'detail': 'Пароль успешно изменен.'}, status=status.HTTP_200_OK)
 
-        except (TypeError, ValueError, OverflowError, get_user_model().DoesNotExist):
-            pass
+        except Exception as e:
+            return Response({"detail": str(e)})
 
     return Response({'detail': 'Недействительный токен или несоответствие пароля.'}, status=status.HTTP_400_BAD_REQUEST)
